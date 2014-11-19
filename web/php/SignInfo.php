45c18a8cc180a06115aea325b431427989388f97
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2014-11-19 18:33:39 -0500

    SignInfo updated
    Need to add more attributes

--- web/php/SignInfo.php
+++ web/php/SignInfo.php
@@ -1,9 +1,30 @@
 <?php
-  $first_name = $last_name = $password = $gender = $martial = $email = " ";
-  if (isset($_POST['First_Name']))
-  	$first_name = fix_string($_POST['First_Name']);
-  if (isset($_POST['Last_Name']))
-  	$last_name = fix_string($_POST['Last_Name']);
+  $name = $password = $gender = $martial = $email = " ";
+  if (isset($_POST['Name']))
+  	$name = fix_string($_POST['Name']);
   if (isset($_POST['Password']))
   	$password = fix_string($_POST['Password']);
-  if
+  if (isset($_POST['Email']))
+  	$email = fix_string($_POST['Email']);
+
+  $fail = validate_name($name);
+  $fail .= validate_password($password);
+  $fail .= validate_email($email);
+  
+  //validate information
+  if($fail == ""){
+  echo "<html><head><title>Successful Registration</title></head></html>";
+  echo "<html><body>You've registered for our system successfully! Now you can enjoy shoppin here!</body></html>";
+
+  //Enter the posted fields into a database
+  require_once 'login.php';
+  $db_server = mysql_connect($db_hostname, $db_username, $db_password);
+
+  if (!db_server) die("Unable to connect to MySQL:". mysql_error());
+
+  $insertQuery = "INSERT INTO UserInfo(name, password, email) VALUES ($name, $password, $email)";
+  $result = mysql_query($insertQuery);
+  if(!result) die ("Database access failed:" . mysql_error());
+
+  ?>
+}
