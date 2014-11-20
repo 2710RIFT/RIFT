0b70a0a205523fd771f08cdd4cf0a442751b72c5
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2014-11-20 16:03:06 -0500

    signinfo
    singinfo

--- web/php/SignInfo.php
+++ web/php/SignInfo.php
@@ -1,15 +1,48 @@
 <?php
-  $name = $password = $gender = $martial = $email = " ";
+  $fname = $lname = $password = $gender = $marital = $email = $bday = $city = $zip = $state = $street = $type = $category = $gross =$name = " ";
   if (isset($_POST['Name']))
   	$name = fix_string($_POST['Name']);
   if (isset($_POST['Password']))
   	$password = fix_string($_POST['Password']);
   if (isset($_POST['Email']))
   	$email = fix_string($_POST['Email']);
+  if (isset($_POST)['gender'])
+    $gender = fix_string($_POST['gender']);
+  if (isset($_POST['marital']))
+    $marital = fix_string($_POST)['marital'];
+  if (isset($_POST['bday']))
+    $bday = fix_string($_POST['bday']);
+  if (isset($_POST['city']))
+    $city = fix_string($_POST['city']);
+  if (isset($_POST['zip']))
+    $zip = fix_string($_POST['zip']);
+  if (isset($_POST['state']))
+    $state = fix_string($_POST['state']);
+  if (isset($_POST['street']))
+    $street = fix_string($_POST['street']);
+  if (isset($_POST['type']))
+    $type = fix_string($_POST['type']);
+  if (isset($_POST['category']))
+    $category = fix_string($_POST['category']);
+  if (isset($_POST['gross']))
+    $gross = fix_string($_POST['gross']);
+  if (isset($_POST['name']))
+    $name = fix_string($_POST['name']);
 
   $fail = validate_name($name);
   $fail .= validate_password($password);
   $fail .= validate_email($email);
+  $fail .= validate_gender($gender);
+  $fail .= validate_marital($marital);
+  $fail .= validate_marital($bday);
+  $fail .= validate_city($city);
+  $fail .= validate_zip($zip);
+  $fail .= validate_state($state);
+  $fail .= validate_street($street);
+  $fail .= validate_type($type);
+  $fail .= validate_category($category);
+  $fail .= validate_gross($gross);
+  $fail .= validate_name($name);
   
   //validate information
   if($fail == ""){
@@ -23,9 +56,15 @@
   if (!db_server) die("Unable to connect to MySQL: " . mysql_error());
   mysql_select_db($db_database) or die("Unable to select database: " . mysql_error() );
 
-  $insertQuery = "INSERT INTO UserInfo(name, password, email) VALUES ($name, $password, $email)";
-  $result = mysql_query($insertQuery);
-  if(!result) die ("Database access failed:" . mysql_error());
+  if($type == Company){
+   $insertQuery = "INSERT INTO UserInfoCom(name, password, email, street, city, state, zip, gross_income, category) VALUES ($name, $password, $email, $street, $city, $state, $zip, $gross, $category)";
+   $result = mysql_query($insertQuery);
+   if(!result) die ("Database access failed:" . mysql_error());
+  }
+  else{
+    $insertQuery ="INSERT INTO UserInfoHome(fname, lname, password, email, street, city, state, zip, gender, bday, marital) VALUES ($fname, $lname, $email, $street, $city, $state, $zip, $gender, $bday, $marital)";
+
+  }
 
   ?>
 }
